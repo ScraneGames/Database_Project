fd925f7426484b6c473b587072e6956d4efdf757
stevethepirate88 <stephen.crane.umd@gmail.com>
2022-12-05 21:35:55 -0500

    Adding Patient Forms
    Created basic add patient form

--- add-patient.php
+++ add-patient.php
@@ -1,1 +1,49 @@
-# What gets called to add a patient
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <title>GFG- Store Data</title>
+   </head>
+   <body>
+      <center>
+         <h1>Adding a Patient</h1>
+         <form action="insert-patient.php" method="post">
+             
+<p>
+               <label for="patient_name">Patient Name:</label>
+               <input type="text" name="patient_name" id="patient_name">
+            </p>
+ 
+             
+<p>
+               <label for="ssn">Social Security Number:</label>
+               <input type="text" name="ssn" id="ssn">
+            </p>
+ 
+             
+<p>
+               <label for="gender">Gender:</label>
+               <input type="text" name="gender" id="gender">
+            </p>
+ 
+             
+<p>
+               <label for="address">address:</label>
+               <input type="text" name="address" id="address">
+            </p>
+ 
+             
+<p>
+               <label for="telephone_number">Telephone Number:</label>
+               <input type="text" name="telephone_number" id="telephone_number">
+            </p>
+
+<p>
+                <label for="dob">Date of Birth:</label>
+                <input type="text" name="dob" id="dob">
+            </p>
+ 
+            <input type="submit" value="Submit">
+         </form>
+      </center>
+   </body>
+</html>

7e39babca927c1693723d8d165ea52dcab7d5fd3
stevethepirate88 <stephen.crane.umd@gmail.com>
2022-12-11 16:20:37 -0500

    fixed the column name
    Fixed the column name to fk_physician_own_ownership_id

--- Clinic_Management/insert-staff.php
+++ Clinic_Management/insert-staff.php
@@ -62,7 +62,7 @@ include "/var/www/html/functions.php";
                     VALUES ('$employee_name', '$shares'); ";
             $sql .= "INSERT INTO physicians (employee_id, position, specialty, employee_name)
                     VALUES ((SELECT employee_id FROM staff WHERE ssn = '$ssn'), '$position', '$specialty', '$employee_name'); ";
-            $sql .= "INSERT INTO physician_owners (fk_own_physician_id, fk_own_employee_name, fk_own_owner_id)
+            $sql .= "INSERT INTO physician_owners (fk_own_physician_id, fk_own_employee_name, fk_physician_own_ownership_id)
                     VALUES ((SELECT physician_id FROM physicians WHERE employee_id = (SELECT employee_id FROM staff WHERE ssn = '$ssn')), '$employee_name', (SELECT ownership_id FROM owners WHERE fk_owner_name = '$employee_name' AND shares = '$shares'))";
 //            } else {
 //                $sql = "INSERT INTO staff employee_name, ssn, gender, position, address, telephone_number)

0575f607206b8d29a99fc253547128bc7110a307
stevethepirate88 <stephen.crane.umd@gmail.com>
2022-12-14 20:16:35 -0500

    new formatting
    I forgot parenthesis, didn't I?

--- In-Patient_Management/Schedule_Surgery/insert-schedule-surgery.php
+++ In-Patient_Management/Schedule_Surgery/insert-schedule-surgery.php
@@ -53,7 +53,14 @@ include "/var/www/html/functions.php";
         $sql = "INSERT INTO surgery_schedule (patient_id, operating_theater, fk_schedule_surgery_code, fk_schedule_surgeon_id, fk_nurse_id_1, fk_nurse_id_2, date, time)
             VALUES ('$patient', '$operating_theater', '$surgery_type', '$surgeon', '$nurse1', '$nurse2', '$date', '$time'); ";
         $sql .= "INSERT INTO recorded_surgeries (fk_recorded_suregon_id, fk_recorded_surgery_code, fk_recorded_patient_id, surgery_id, date)
-            VALUES ('$surgeon', '$surgery_type', '$patient', (SELECT surgery_id FROM surgery_schedule WHERE patient_id = '$patient' AND fk_schedule_surgery_code = '$surgery_type' AND fk_schedule_surgeon_id = '$surgeon' AND date = '$date' AND time = '$time'), '$date'";
+            VALUES ('$surgeon', '$surgery_type', '$patient',
+                (SELECT surgery_id FROM surgery_schedule
+                    WHERE patient_id = '$patient'
+                        AND fk_schedule_surgery_code = '$surgery_type'
+                        AND fk_schedule_surgeon_id = '$surgeon'
+                        AND date = '$date'
+                        AND time = '$time'),
+            '$date')";
 
         if(mysqli_multi_query($conn, $sql)){
             echo "<h3>Surgery Scheduled Successfully.</h3>";

5cf707a08ebfb64ce04942a5986396c9b591ade5
stevethepirate88 <scrane@scranegames.tv>
2022-12-15 23:09:35 -0500

    Update schedule-surgery-team.php

--- In-Patient_Management/Schedule_Surgery/schedule-surgery-team.php
+++ In-Patient_Management/Schedule_Surgery/schedule-surgery-team.php
@@ -82,7 +82,7 @@ echo "The category is" .$category['category'];
          <input type="hidden" id="operating_theater" name="operating_theater" value= <?php echo "$operating_theater"; ?>>
          <input type="hidden" id="date" name="date" value=<?php echo "$date"; ?>>
          <input type="hidden" id="time" name="time" value=<?php echo "$time"; ?>>
-         <input type="hidden" id="category" name="category" value="O>
+         <input type="hidden" id="category" name="category" value="O">
 <p>
             <label>Select a Surgeon</label>
                     <select name="surgeon">

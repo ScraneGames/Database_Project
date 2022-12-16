1ff45328a9e8463c7080d244af7d6e39a1af5214
stevethepirate88 <scrane@scranegames.tv>
2022-12-16 11:30:33 -0500

    Let's try letting you change your primary physician

--- Patient_Management/Edit_Patient/edit-patient.php
+++ Patient_Management/Edit_Patient/edit-patient.php
@@ -14,6 +14,30 @@ $sql = "SELECT * FROM patient_personal_data WHERE patient_id = '$original_patien
 $result = mysqli_query($conn,$sql);
 
 $user = mysqli_fetch_array($result,MYSQLI_ASSOC);
+
+$primary_id = $user['primary_physician_id'];
+
+$primary_sql = "SELECT employee_name FROM physicians WHERE physician_id = '$primary_id";
+
+$sql_find_phyisicans_less_7 = "SELECT physicians.employee_name, physicians.physician_id
+                        FROM physicians
+                        LEFT OUTER JOIN patient_primary
+                        ON physicians.physician_id = patient_primary.fk_primary_physician_id
+                        WHERE physicians.position <> 'chief_of_staff'
+                        GROUP BY physicians.physician_id
+                        HAVING COUNT(patient_primary.fk_primary_physician_id) < 7";
+
+$sql_find_physicians_less_7_result = mysqli_query($conn,$sql_find_phyisicans_less_7);
+
+$sql_find_phyisicans_less_20 = "SELECT physicians.employee_name, physicians.physician_id
+                        FROM physicians
+                        LEFT OUTER JOIN patient_primary
+                        ON physicians.physician_id = patient_primary.fk_primary_physician_id
+                        WHERE physicians.position <> 'chief_of_staff'
+                        GROUP BY physicians.physician_id
+                        HAVING COUNT(patient_primary.fk_primary_physician_id) < 20";
+
+$sql_find_physicians_less_20_result = mysqli_query($conn,$sql_find_phyisicans_less_20);
 ?>
 
 <!DOCTYPE html>
@@ -65,6 +89,60 @@ $user = mysqli_fetch_array($result,MYSQLI_ASSOC);
                 <label for="dob">Date of Birth:</label>
                 <input type="date" name="dob" value="<?php echo $user['dob']; ?>" id="dob">
             </p>
+<p>
+<?php
+echo "<h3>The patient's current primary physician is $primary. </h3>"
+?>
+</p>
+<?php if (mysqli_num_rows($sql_find_physicians_less_7_result) > 0): ?>
+<p>
+               <label for="primary_less_7">Primary Physician:</label>
+               <select name="primary_less_7">
+            <?php
+                // use a while loop to fetch data
+                // from the $all_categories variable
+                // and individually display as an option
+                while ($primary_less_7 = mysqli_fetch_array(
+                  $sql_find_physicians_less_7_result,MYSQLI_ASSOC)):;
+            ?>
+                <option value="<?php echo $primary_less_7["physician_id"];
+                    // The value we usually set is the primary key
+                ?>">
+                    <?php echo $primary_less_7["employee_name"] . " ".$primary_less_7["physician_id"];
+                        // To show the employee name to the user
+                    ?>
+                </option>
+               <?php
+               endwhile;
+               // While loop must be terminated
+         ?>
+      </select>
+</p>
+      <?php elseif (mysqli_num_rows($sql_find_physicians_less_20_result) > 0): ?>
+<p>
+         <label for="primary_less_20">Primary Physician:</label>
+                        <select name="primary_less_20">
+                     <?php
+                        // use a while loop to fetch data
+                        // from the $all_categories variable
+                        // and individually display as an option
+                        while ($primary_less_20 = mysqli_fetch_array(
+                           $sql_find_physicians_less_20_result,MYSQLI_ASSOC)):;
+                     ?>
+                        <option value="<?php echo $primary_less_20["physician_id"];
+                           // The value we usually set is the primary key
+                        ?>">
+                           <?php echo $primary_less_20["employee_name"] . " ".$primary_less_20["physician_id"];
+                                 // To show the employee name to the user
+                           ?>
+                        </option>
+                        <?php
+                        endwhile;
+                        // While loop must be terminated
+                        ?>
+      </select>
+      <?php endif; ?>
+      <br>
 
             <input type="submit" value="Submit">
          </form>

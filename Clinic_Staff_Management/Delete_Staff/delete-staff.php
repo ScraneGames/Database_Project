eb9644a99693a84874bb8bf1191b05a70b12db94
stevethepirate88 <scrane@scranegames.tv>
2022-12-16 22:10:41 -0500

    deleting is wonky

--- Clinic_Staff_Management/Delete_Staff/delete-staff.php
+++ Clinic_Staff_Management/Delete_Staff/delete-staff.php
@@ -32,18 +32,33 @@ $owner_result = mysqli_query($conn,$owner_sql);
 $primary_check_sql = "SELECT primary_physician_id FROM patient_medical_data WHERE primary_physician_id = (SELECT physician_id FROM physicians WHERE employee_id = '$original_employee_id')";
 $primary_check_result = mysqli_query($conn,$primary_check_sql);
 
+$primary_array = mysqli_fetch_array($primary_check_result);
+
+$primary_id = $primary_array['primary_physician_id'];
+
 $inpatient_check_sql = "SELECT attending_physician_id FROM inpatients WHERE attending_physician_id = (SELECT physician_id FROM physicians WHERE employee_id = '$original_employee_id')";
 $inpatient_check_sql_result = mysqli_query($conn,$inpatient_check_sql);
 
+
+// delete nurse
+
 if ($user['position'] == "nurse"){
         $delete_sql = "DELETE FROM staff WHERE employee_id = '$original_employee_id'";
-    } elseif ($user['position'] == "surgeon") {
+    }
+    // delete surgeon
+    elseif ($user['position'] == "surgeon") {
         $delete_sql = "DELETE FROM staff WHERE employee_id = '$original_employee_id'";
-        } elseif ($user['position'] == "physician" ) {
+        }
+        // delte physician
+
+        elseif ($user['position'] == "physician" ) {
+
+            // Check if a primary physician and if so, replace the primary physician id in patient_medical data
+            // with the physician id of the chief of staff
             if (mysqli_num_rows($primary_check_result) > 0) {
                 $replace_primary_sql = "UPDATE patient_medical_data
                                         SET primary_physician_id = (SELECT physician_id FROM physicians WHERE position = 'chief_of_staff')
-                                        WHERE primary_physician_id = '$original_employee_id'";
+                                        WHERE primary_physician_id = '$primary_id'";
                 if (mysqli_query($conn, $replace_primary_sql)) {
                     echo "Replaced Existing Primary Physicians With Chief of Staff Correctly";
                     echo "<br>";
@@ -52,10 +67,13 @@ if ($user['position'] == "nurse"){
                 }
             }
 
+            // Check if an attending and if so, replace the primary physician id in patient_medical data
+            // with the physician id of the chief of staff
+
             if (mysqli_num_rows($inpatient_check_sql_result) > 0) {
                 $replace_attending_sql = "UPDATE inpatients
                                         SET attending_physician_id = (SELECT physician_id FROM physicians WHERE position = 'chief_of_staff')
-                                        WHERE primary_physician_id = '$original_employee_id'";
+                                        WHERE primary_physician_id = '$primary_id'";
                 if (mysqli_query($conn, $replace_attending_sql)) {
                     echo "Replaced Existing Primary Physicians With Chief of Staff Correctly";
                     echo "<br>";
@@ -63,6 +81,8 @@ if ($user['position'] == "nurse"){
                     echo "Error: " . $replace_attending_sql . "<br>" . $conn->error;
                 }
             }
+
+            // Check if an owner and if so, delete physician from the owners table
                 if (mysqli_num_rows($owner_result) > 0) {
                     $delete_owner_sql = "DELETE FROM owners WHERE ownership_id =
                     (SELECT fk_physician_own_ownership_id
@@ -78,16 +98,35 @@ if ($user['position'] == "nurse"){
                         echo "Error: " . $sql . "<br>" . $conn->error;
                     }
                 }
+
+                // Set Delete SQL
                 $delete_sql = "DELETE FROM staff WHERE employee_id = '$original_employee_id'";
+
+
                 } elseif ($user['position'] == "janitor" || $user['position'] == "secretary") {
                 $delete_sql = "DELETE FROM staff WHERE employee_id = '$original_employee_id'";
-            } elseif ($user['position'] == "chief_of_staff") {
+            }
+
+            // Delete Chief of Staff
+
+            elseif ($user['position'] == "chief_of_staff") {
+
+                // Check if chief is a primary physician, and if so, throw an error
+
                 if (mysqli_num_rows($primary_check_result) > 0) {
                     echo "<br>";
                     echo "The chief of staff has primary patients assigned. Please reassign those patients to new primary doctors before proceeeding.";
                     echo "<br>";
 
-                } else {
+                } elseif {
+                    if (mysqli_num_rows($inpatient_check_sql_result) > 0) {
+                        echo "<br>";
+                        echo "The chief of staff has inpatients assigned. Please reassign those patients to new primary doctors before proceeeding.";
+                        echo "<br>";
+                } {
+
+                    // Check if the chief is a n owner, and if so, delete from owners
+
                     if (mysqli_num_rows($owner_result) > 0) {
                         $delete_owner_sql = "DELETE FROM owners WHERE ownership_id =
                         (SELECT fk_physician_own_ownership_id
---
+++ In-Patient_Management/Views/view-all-inpatients.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>All Inpatients</title>
+</head>
+
+
+<body>
+    <center>
+        <?php
+
+
+
+        // check Connection
+include "/var/www/html/functions.php";
+
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+
+        // Taking all the values from the patient-administration.php
+
+        $sql_find_beds = "SELECT patient_name, employee_name, patient_id, physician_id, position, bed_id, nursing_unit, wing, room_number, bed_number
+                        FROM beds
+                        JOIN inpatients
+                        ON beds.bed_id = inpatients.fk_inpatients_bed_id
+                        JOIN physicians
+                        ON inpatients.attending_physician_id = physicians.physician_id
+                        JOIN patient_personal_data
+                        ON inpatients.fk_inpatients_patient_id = patient_personal_data.patient_id";
+        $result = mysqli_query($conn,$sql_find_beds);
+
+echo "All Beds Without Assigned Patients";
+echo "<br>";
+echo "<table border='1'>
+<tr>
+<th>Bed ID</th>
+<th>Nursing Unit</th>
+<th>Wing</th>
+<th>Room Number/th>
+<th>Bed Letter</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result)){
+    echo "<tr>";
+    echo "<td>" . $row['bed_id'] . "</td>";
+    echo "<td>" . $row['nursing_unit'] . "</td>";
+    echo "<td>" . $row['wing'] . "</td>";
+    echo "<td>" . $row['room_number'] . "</td>";
+    echo "<td>" . $row['bed_number'] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+
+echo "<br>";
+
+?>
+
+        </center>
+    </body>
+</html>
